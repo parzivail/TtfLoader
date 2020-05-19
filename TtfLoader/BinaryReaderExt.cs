584b5a7484dfb8c9e57afcc673bda13b28fa5226
parzivail <colby.newman2000@gmail.com>
2020-05-19 17:03:36 -0400

    Add project files.

---
+++ TtfLoader/BinaryReaderExt.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace TtfLoader
+{
+    internal static class BinaryReaderExt
+    {
+        public static T Read<T>(this BinaryReader r) where T : IBinarySerializable
+        {
+            var t = Activator.CreateInstance<T>();
+            t.Read(r);
+            return t;
+        }
+
+        public static T[] ReadMany<T>(this BinaryReader r, int count) where T : IBinarySerializable
+        {
+            var t = new T[count];
+
+            for (var i = 0; i < count; i++) t[i] = r.Read<T>();
+
+            return t;
+        }
+    }
+}
